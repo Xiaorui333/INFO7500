55958f8c2c406d55dd3ca0cea520d8956100f55a
suhabe <suhabe@bugrara.com>
2025-04-03 15:08:10 -0700

    remove target dir from digital signatures

--- digital_signatures/target/debug/deps/cfg_if-a608de5e335e92d9.d
+++
@@ -1,7 +0,0 @@
-/Users/frida/Documents/INFO7500/digital_signatures/target/debug/deps/libcfg_if-a608de5e335e92d9.rmeta: /Users/frida/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/frida/Documents/INFO7500/digital_signatures/target/debug/deps/libcfg_if-a608de5e335e92d9.rlib: /Users/frida/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/frida/Documents/INFO7500/digital_signatures/target/debug/deps/cfg_if-a608de5e335e92d9.d: /Users/frida/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/frida/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
